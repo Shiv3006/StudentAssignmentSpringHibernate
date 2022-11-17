7437a44e60d3f04b1aeb9d5dfacd3a6f77a54925
Shiv3006 <116787360+Shiv3006@users.noreply.github.com>
2022-11-17 18:56:32 +0530

    Add files via upload

---
+++ StudentNotFoundException.java
@@ -0,0 +1,31 @@
+package com.bnt.exception;
+
+public class StudentNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public StudentNotFoundException() {
+		super();
+		
+	}
+
+	public StudentNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		
+	}
+
+	public StudentNotFoundException(String message) {
+		super(message);
+		
+	}
+
+	public StudentNotFoundException(Throwable cause) {
+		super(cause);
+		
+	}
+
+	
+}
